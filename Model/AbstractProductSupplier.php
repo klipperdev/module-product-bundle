11aec91063afca0efa77753e1c5b893d536bd77c
François Pluchino <francois.pluchino@klipper.dev>
2020-10-16 11:12:11 +0200

    Add product supplier model

---
+++ Model/AbstractProductSupplier.php
@@ -0,0 +1,96 @@
+<?php
+
+/*
+ * This file is part of the Klipper package.
+ *
+ * (c) François Pluchino <francois.pluchino@klipper.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Klipper\Module\ProductBundle\Model;
+
+use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation as Serializer;
+use Klipper\Component\Model\Traits\OrganizationalRequiredTrait;
+use Klipper\Component\Model\Traits\TimestampableTrait;
+use Klipper\Component\Model\Traits\UserTrackableTrait;
+use Klipper\Module\PartnerBundle\Model\AccountInterface;
+use Klipper\Module\ProductBundle\Model\Traits\ProductableTrait;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Product supplier model.
+ *
+ * @author François Pluchino <francois.pluchino@klipper.dev>
+ */
+abstract class AbstractProductSupplier implements ProductSupplierInterface
+{
+    use OrganizationalRequiredTrait;
+    use TimestampableTrait;
+    use UserTrackableTrait;
+    use ProductableTrait;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     *
+     * @Assert\Type(type="string")
+     * @Assert\Length(min="0", max="255")
+     *
+     * @Serializer\Expose
+     */
+    protected ?string $reference = null;
+
+    /**
+     * @ORM\Column(type="float", nullable=true)
+     *
+     * @Assert\Type(type="float")
+     *
+     * @Serializer\Expose
+     */
+    protected ?float $quantity = null;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Klipper\Module\PartnerBundle\Model\AccountInterface")
+     *
+     * @Serializer\Expose
+     */
+    protected ?AccountInterface $supplier = null;
+
+    public function setReference(?string $reference): self
+    {
+        $this->reference = $reference;
+
+        return $this;
+    }
+
+    public function getReference(): ?string
+    {
+        return $this->reference;
+    }
+
+    public function setQuantity(?float $quantity): self
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
+    public function getQuantity(): ?float
+    {
+        return $this->quantity;
+    }
+
+    public function setSupplier(?AccountInterface $supplier): self
+    {
+        $this->supplier = $supplier;
+
+        return $this;
+    }
+
+    public function getSupplier(): ?AccountInterface
+    {
+        return $this->supplier;
+    }
+}
---
+++ Model/ProductSupplierInterface.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * This file is part of the Klipper package.
+ *
+ * (c) François Pluchino <francois.pluchino@klipper.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Klipper\Module\ProductBundle\Model;
+
+use Klipper\Component\Model\Traits\OrganizationalRequiredInterface;
+use Klipper\Component\Model\Traits\TimestampableInterface;
+use Klipper\Component\Model\Traits\UserTrackableInterface;
+use Klipper\Contracts\Model\IdInterface;
+use Klipper\Module\PartnerBundle\Model\AccountInterface;
+use Klipper\Module\ProductBundle\Model\Traits\ProductableRequiredInterface;
+
+/**
+ * Product supplier interface.
+ *
+ * @author François Pluchino <francois.pluchino@klipper.dev>
+ */
+interface ProductSupplierInterface extends
+    IdInterface,
+    OrganizationalRequiredInterface,
+    ProductableRequiredInterface,
+    TimestampableInterface,
+    UserTrackableInterface
+{
+    /**
+     * @return static
+     */
+    public function setReference(?string $reference);
+
+    public function getReference(): ?string;
+
+    /**
+     * @return static
+     */
+    public function setQuantity(?float $quantity);
+
+    public function getQuantity(): ?float;
+
+    /**
+     * @return static
+     */
+    public function setSupplier(?AccountInterface $supplier);
+
+    public function getSupplier(): ?AccountInterface;
+}
