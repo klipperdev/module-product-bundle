0bd0a9c58d2a0995d47db2dec2fc93bfb4bb3f75
François Pluchino <francois.pluchino@klipper.dev>
2020-10-25 14:51:19 +0100

    Add product combinationable trait

---
+++ Model/Traits/ProductCombinationableTrait.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * This file is part of the Klipper package.
+ *
+ * (c) François Pluchino <francois.pluchino@klipper.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Klipper\Module\ProductBundle\Model\Traits;
+
+use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation as Serializer;
+use Klipper\Module\ProductBundle\Model\ProductCombinationInterface;
+
+/**
+ * @author François Pluchino <francois.pluchino@klipper.dev>
+ */
+trait ProductCombinationableTrait
+{
+    /**
+     * @ORM\ManyToOne(
+     *     targetEntity="Klipper\Module\ProductBundle\Model\ProductCombinationInterface",
+     *     fetch="EXTRA_LAZY"
+     * )
+     *
+     * @Serializer\MaxDepth(depth=1)
+     * @Serializer\Expose
+     * @Serializer\ReadOnly
+     */
+    protected ?ProductCombinationInterface $productCombination = null;
+
+    public function getProductCombination(): ?ProductCombinationInterface
+    {
+        return $this->productCombination;
+    }
+
+    public function setProductCombination(?ProductCombinationInterface $productCombination): self
+    {
+        $this->productCombination = $productCombination;
+
+        return $this;
+    }
+
+    public function getProductCombinationId()
+    {
+        return null !== $this->getProductCombination()
+            ? $this->getProductCombination()->getId()
+            : null;
+    }
+}
