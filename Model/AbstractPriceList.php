de2765e511e24df2d3cc12462becf147e0b51b00
François Pluchino <francois.pluchino@klipper.dev>
2020-11-16 14:47:36 +0100

    Make required the label field of price list

--- Model/AbstractPriceList.php
+++ Model/AbstractPriceList.php
@@ -11,12 +11,14 @@
 
 namespace Klipper\Module\ProductBundle\Model;
 
+use Doctrine\ORM\Mapping as ORM;
 use JMS\Serializer\Annotation as Serializer;
 use Klipper\Component\Model\Traits\CurrencyableTrait;
 use Klipper\Component\Model\Traits\LabelableTrait;
 use Klipper\Component\Model\Traits\OrganizationalRequiredTrait;
 use Klipper\Component\Model\Traits\TimestampableTrait;
 use Klipper\Component\Model\Traits\UserTrackableTrait;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * Price list model.
@@ -32,4 +34,15 @@ abstract class AbstractPriceList implements PriceListInterface
     use OrganizationalRequiredTrait;
     use TimestampableTrait;
     use UserTrackableTrait;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     *
+     * @Assert\Type(type="string")
+     * @Assert\Length(max=255)
+     * @Assert\NotBlank
+     *
+     * @Serializer\Expose
+     */
+    protected ?string $label = null;
 }
