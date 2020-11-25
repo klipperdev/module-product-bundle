b07a62c5d6aca7a2ac9e9e91e26032902a152d29
François Pluchino <francois.pluchino@klipper.dev>
2020-11-25 14:01:24 +0100

    Update the serializer max depth checks on product combination model

--- Model/Traits/ProductCombinationableTrait.php
+++ Model/Traits/ProductCombinationableTrait.php
@@ -27,7 +27,7 @@ trait ProductCombinationableTrait
      * )
      *
      * @Serializer\Expose
-     * @Serializer\MaxDepth(1)
+     * @Serializer\MaxDepth(3)
      */
     protected ?ProductCombinationInterface $productCombination = null;
 
