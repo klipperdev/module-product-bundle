4be0bd997545d6a7054919f189463ead6d9048c2
François Pluchino <francois.pluchino@klipper.dev>
2021-07-04 12:17:46 +0200

    Add sortable group on product field in product combination

--- Model/AbstractProductCombination.php
+++ Model/AbstractProductCombination.php
@@ -14,6 +14,7 @@ namespace Klipper\Module\ProductBundle\Model;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
 use JMS\Serializer\Annotation as Serializer;
 use Klipper\Component\Model\Traits\OrganizationalRequiredTrait;
 use Klipper\Component\Model\Traits\SortableTrait;
@@ -45,6 +46,8 @@ abstract class AbstractProductCombination implements ProductCombinationInterface
      * @ORM\JoinColumn(onDelete="CASCADE")
      *
      * @Assert\NotBlank
+     *
+     * @Gedmo\SortableGroup
      *
      * @Serializer\Expose
      * @Serializer\MaxDepth(1)
