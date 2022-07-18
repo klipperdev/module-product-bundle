5d5acdaced32d25bffa9b5b54cc75699439bc504
François Pluchino <francois.pluchino@klipper.dev>
2022-07-18 11:07:03 +0200

    Add product family model

---
+++ Model/AbstractProductFamily.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of the Klipper package.
+ *
+ * (c) François Pluchino <francois.pluchino@klipper.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Klipper\Module\ProductBundle\Model;
+
+use JMS\Serializer\Annotation as Serializer;
+use Klipper\Component\Model\Traits\LabelableTrait;
+use Klipper\Component\Model\Traits\NameableTrait;
+use Klipper\Component\Model\Traits\OrganizationalRequiredTrait;
+use Klipper\Component\Model\Traits\TimestampableTrait;
+use Klipper\Component\Model\Traits\UserTrackableTrait;
+
+/**
+ * Product family model.
+ *
+ * @author François Pluchino <francois.pluchino@klipper.dev>
+ *
+ * @Serializer\ExclusionPolicy("all")
+ */
+abstract class AbstractProductFamily implements ProductFamilyInterface
+{
+    use LabelableTrait;
+    use NameableTrait;
+    use OrganizationalRequiredTrait;
+    use TimestampableTrait;
+    use UserTrackableTrait;
+}
